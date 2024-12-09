66a01bea99c01afb1379cd16141dc2a3ee78e45b
Muhammad Eri Setyawan <erisetyawan34@gmail.com>
2024-12-09 12:05:25 +0700

    fix

--- api/index.ts
+++ api/index.ts
@@ -23,9 +23,6 @@ app.get('/', (req: any, res: any) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+const PORT = 3001;
 
 module.exports = app;
